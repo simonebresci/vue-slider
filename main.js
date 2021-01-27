e6553e1ca8145902e512ac3cb2e46b42a1f2a3da
Simone <simone.bresci.05@gmail.com>
2021-01-27 17:20:11 +0100

    grafica cerchietti

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ var app = new Vue({
       'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
       'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
       'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
-      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg',
+      'https://images.wired.it/wp-content/uploads/2020/09/01135456/2448_Blue_Moon_Airplane_1280.jpg',
+      'https://www.columbusassicurazioni.it/media/31932/vacanze-mare-estero.jpg?width=800'
     ],
   },
   methods:{
@@ -15,10 +17,14 @@ var app = new Vue({
         alert('ciao');
       },
       ImgIndexAdd: function(){
-        this.currentImgIndex += 1;
+        if (this.currentImgIndex < this.pathImmagini.length-1){
+          this.currentImgIndex += 1;
+        }
       },
       ImgIndexSub: function(){
-        this.currentImgIndex -= 1;
+        if (this.currentImgIndex>=1){
+          this.currentImgIndex -= 1;
+        }
       }
 
 

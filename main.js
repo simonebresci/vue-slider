28b7d4665aff5fd8493bdcb435b6301026ff6de3
Simone <simone.bresci.05@gmail.com>
2021-01-27 16:45:13 +0100

    Scorrimento immagini in debug mode

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 var app = new Vue({
   el: "#root" ,
   data: {
-    colore: 'blue'
-  }
+    currentImgIndex: 0,
 
+    pathImmagini: [
+      'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+    ],
+  },
+  methods:{
+      ciao: function(){
+        alert('ciao');
+      },
+      ImgIndexAdd: function(){
+        this.currentImgIndex += 1;
+      },
+      ImgIndexSub: function(){
+        this.currentImgIndex -= 1;
+      }
+
+
+
+  }
+  // /Data
 
 
 });
+// /Vue
 // Abilita i dev tools
 Vue.config.devtools = true;
